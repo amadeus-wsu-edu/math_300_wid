599c46df3e348f854c1dc4e423eef845c6b60ed4
amadeus-wsu-edu <amadeus.widjaja@wsu.edu>
2022-07-12 11:00:44 -0700

    jul 12

--- matlab/Math300_2.m
+++ matlab/Math300_2.m
@@ -3,3 +3,82 @@ B = [5 6 ; 7 8];
 
 C = A*B; 
 D = A.*B;
+
+E = A.^2;
+
+F = A^2;
+
+A1 = [1 2 ; 3 4 ; 5 6];
+
+C2 = A';
+
+D1 = A';
+
+matrix1 = [1,1,1;1,1,1;1,1,1];
+matrix2 = [0,0,0;0,0,0;0,0,0];
+matrix3 = [1,0,0;0,1,0;0,0,1];
+
+v = 1:10;
+w = 1:2:10;
+
+matrix4 = [1,2,3;4,5,6;7,8,9];
+disp(matrix4(2,2));
+
+disp(matrix4(2,1:3));
+disp(matrix4(1:3,1));
+
+numa = rand;
+message = ['The result is: ' num2str(numa)];
+disp(message);
+
+%% variables
+a=3;
+
+%% Code
+if a > 0
+    x = 0;
+elseif a < 0
+    x = 1;
+else
+    x = 2;
+end
+
+%% Code 2
+N = 10;
+for a1 = 1:N
+    disp('a1');
+end 
+
+vector1 = 1:10;
+sum = 0;
+for j = vector1
+    sum = sum + j;
+end
+
+%% while loops
+
+randomx = randi(1000);
+disp(randomx)
+
+while randomx <= 500
+    randomx = randi(1000);
+    disp(randomx)
+end
+
+% && vs ||
+% && == AND, || == OR
+
+
+%% 11jul
+failed = true;
+numoftries = 0;
+
+while failed
+    numoftries = numoftries + 1;
+    dice1 = randi(6);
+    dice2 = randi(6);
+    if dice1 == 6 && dice1 == dice2
+        failed = false;
+    end
+end
+disp(numoftries);
---
+++ matlab/Math300hw2.m
@@ -0,0 +1,45 @@
+%% question 1
+
+A1 = [2 -1 4
+    9 6 2
+    1 3 8];
+
+B1 = [1 1 1
+    1 1 1
+    1 1 1 ];
+
+X1 = [3
+    6
+    8];
+
+Y1 = [5 10 15];
+
+% A * B
+q1p1 = A1 * B1;
+
+% A * x
+q1p2 = A1 * X1;
+
+% x' * B
+q1p3 = X1' * B1;
+
+% B * y
+q1p4 = B1 * Y1;
+
+% x * A
+
+% x * Y
+
+% y * X
+
+% x * Y'
+
+% x .* y
+
+% A .* B
+
+
+%% question 2
+%% question 3
+%% question 4
+%% question 5
